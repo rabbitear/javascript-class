288b6829776f109fb7087d933ff07c912b8a2c13
rabbitear <weatchu@gmail.com>
2014-11-23 20:55:08 -0900

    added ytsearch.html in Module 13

---
+++ public/Mod13/ytsearch.js
@@ -0,0 +1,36 @@
+// Helper function to display JavaScript value on HTML page.
+myak = 'AIzaSyDihpaf2iGC3kUp28b-OAvBZr5VgRax7hs';
+
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+}
+
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+// Called automatically when YouTube API interface is loaded.
+function onYouTubeApiLoad() {
+    gapi.client.setApiKey(myak);
+    search();
+}
+
+function search() {
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q: 'animal',
+    });
+    
+    // Send the request to the API server,
+    // and invoke onSearchRepsonse() with the response.
+    request.execute(onSearchResponse);
+}
+
+// Called automatically with the response of the YouTube API request.
+function onSearchResponse(response) {
+    showResponse(response);
+}
+
