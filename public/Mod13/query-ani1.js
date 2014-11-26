8bf10492354dc32c17df0b79efcd57b77c40ffc6
rabbitear <weatchu@gmail.com>
2014-11-25 16:55:46 -0900

    added the pictures of china page

--- public/Mod13/query-ani1.js
+++ public/Mod13/query-ani1.js
@@ -1,6 +1,10 @@
 console.log('Hey!');
 
 $(document).ready(function(){
-    $("#box").css({'height':'100px', 'width':'100px', 'background-color':'black'})
+    $("#box").css({'height':'100px', 'width':'100px', 
+        'background-color':'black', 'position':'absolute'});
+    $("#box").animate({'left':'1000px'},6000,'swing', function(){
+        alert("DONE ANIMATED");
+    });
 });
 
